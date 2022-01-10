fe0e11e98f17e9891a09704f0927a9a1f22a04ec
ke1sukehsgw <chiro9689@gmail.com>
2022-01-10 17:45:09 +0900

    誤字の訂正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
 
-  validates_uniquness_of :tweet_id, scope: :user_id
+  validates_uniqueness_of :tweet_id, scope: :user_id
 end
